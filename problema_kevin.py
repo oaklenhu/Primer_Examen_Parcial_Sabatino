67f3edad4832d3037d3c4716d83be5626299ab64
oaklenhu <100497317+oaklenhu@users.noreply.github.com>
2022-02-27 08:13:40 -0600

    Add files via upload

---
+++ problema_kevin.py
@@ -0,0 +1,8 @@
+
+numero_inicial = int(input("Ingrese un numero cual sea: "))
+sumatoria = 0
+for numero in range(numero_inicial):
+    sumatoria = sumatoria + (numero + 1)
+
+
+print("La sumatoria de los numeros desde el numero inicial hasta el numero final es: ",sumatoria)
